8ab697857926398d0e259bf31d0d6496cb93342f
fourthsemcs4 <trishamaithri@gmail.com>
2024-08-08 11:08:06 +0530

    steps prgm

---
+++ lab1.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+d={"days":[1,2,3,4,5,6,7,8,9,10],"steps":[3300,2200,4400,3204,5678,4567,7890,8675,6789,6789,]}
+dp=pd.DataFrame(d)
+dp["+1000 steps"]=dp["steps"]+1000
+fi=dp[dp["+1000 steps"]>5000]["days"]
+print("Dataframe:",dp)
+print("\n days in which steps were >5000:\n",fi)
